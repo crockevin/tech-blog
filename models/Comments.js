c2c61e81b828061838acef2db9c1db889e32b074
Kevin <crochedengren@gmail.com>
2023-12-17 00:35:21 -0500

    added all handlebars a basic routes to get to them, set up the server.js, Models, connections

---
+++ models/Comments.js
@@ -0,0 +1,45 @@
+const { Model, DataTypes, Sequelize } = require('sequelize')
+const sequelize = require('../config/connection')
+
+class Comments extends Model {}
+
+Comments.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true
+        },
+        content: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        timeStamp: {
+            type: DataTypes.DATE,
+            defaultValue: DataTypes.NOW
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        },
+        blog_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'blog',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comments'
+    }
+)
+
+module.exports = Comments
